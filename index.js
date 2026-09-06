22f5604e0e363306a1fe7e9600e2112dbb982b0c
Rara12-web <mutiaraaviva12@gmail.com>
2026-09-06 13:43:49 +0700

    Add environment variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Backend NourishVision berhasil berjalan!");
+    res.send(`Backend ${process.env.APP_NAME} berhasil berjalan!`);
 });
 
 app.get("/api/hello", (req, res) => {
